c0d339c36026d012fc9f713b7b2f12aecebb9f6e
Maribel Duran <maribelduranucm@gmail.com>
2018-03-08 17:34:00 -0800

    Add more styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,94 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from './components/table'
+
+const PATH_BASE = 'https://fcctop100.herokuapp.com/api/fccusers/top';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      results: null,
+      isLoading: false,
+      searchKey: null,
+      error: null,
+    }
+    this.fetchTopRecentCampers = this.fetchTopRecentCampers.bind(this);
+    this.fetchAllTimeCampers = this.fetchAllTimeCampers.bind(this);
+    //this.setTopRecentCampers =this.setTopRecentCampers.bind(this);
+    this.setCamperResults = this.setCamperResults.bind(this);
+  }
+  
+  setCamperResults(result) {
+    const {searchKey, results } = this.state
+    this.setState({ 
+      results: {
+        ...results, 
+        [searchKey]: result
+      },
+      isLoading: false
+    });
+  }
+
+  //eventually change this to use async await
+  fetchTopRecentCampers() {
+    const searchKey = 'recent';
+    this.setState({
+      isLoading: true,
+      searchKey: searchKey,
+    });
+
+    fetch(`${PATH_BASE}/${searchKey}`)
+    .then(response => response.json())
+    .then(result => this.setCamperResults(result))
+    .catch(e => this.setState({ error: e}));
+  }  
+
+  fetchAllTimeCampers(){
+    const searchKey = 'alltime';
+    this.setState({
+        isLoading: true,
+        searchKey: 'alltime',
+    });
+    fetch(`${PATH_BASE}/${searchKey}`)
+    .then(response => response.json())
+    .then(result => this.setCamperResults(result))
+    .catch(e => this.setState({ error: e}));
+  }
+
+  componentDidMount() {
+    this.fetchAllTimeCampers();
+    this.fetchTopRecentCampers();
+  }
+
   render() {
-    return (
+    const {isLoading, results, searchKey} = this.state;
+    console.log(this.state);
+
+    const list = (
+      results &&
+      results[searchKey] 
+    ) || [];
+      
+      return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">FCC Camper Leaderboard</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="page">
+          <div className="interactions">
+            { isLoading  
+            ?  <p>Loading...</p> 
+              :
+              null
+            }
+          </div>
+          <div className="interactions">
+            <Table list={list}/>
+          </div>
+      </div>
       </div>
     );
   }
