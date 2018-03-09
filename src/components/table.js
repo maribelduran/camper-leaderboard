e6a5c53bad44e0c1ad1a20847fcbda84e58ca7e9
Maribel Duran <maribelduranucm@gmail.com>
2018-03-09 11:31:27 -0800

    Add users image and url

--- src/components/table.js
+++ src/components/table.js
@@ -9,10 +9,12 @@ const Table = ({list, onSortSelect}) =>
     <span className="clmn-width-40"><button onClick={() => onSortSelect("recent")} >Points in Past 30 Days</button></span>
     <span className="clmn-width-40"><button onClick={() => onSortSelect("alltime")} >All Time Points</button></span>
   </div>
-  {list.map((item,index) => 
+  {list.map((item,index) =>  
     <div key={item.username} className="table-row">
       <span className="clmn-width-10">{index+1}</span>
-      <span className="clmn-width-40">{item.username}</span>
+      <span className="clmn-width-40">
+        <img src={item.img}/>
+        <a href={`https://www.freecodecamp.org/${item.username}`} target="_blank">{item.username}</a></span>
       <span className="clmn-width-40">{item.recent}</span>
       <span className="clmn-width-40">{item.alltime}</span>
     </div>
