f5aa54c15e5705b7f9170a7e56c406b3a8a4a800
Maribel Duran <maribelduranucm@gmail.com>
2018-03-09 14:46:51 -0800

    Add loading component

---
+++ src/components/loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Loading = () => 
+//<div>Loading...</div>
+<div>
+<i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+<span className="sr-only">Loading...</span>
+</div>
+
+export default Loading;
